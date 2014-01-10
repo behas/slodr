43b1d2dc2654eb01cd35bbf2b0888d5636155519
Cesare Concordia <cesare@concordiamac.isti.cnr.it>
2014-01-10 11:41:57 +0100

    sparql endpoint changed, query answer processing changed.

--- src/main/java/eu/europeana/lod/data/EuropeanaResponse.java
+++ src/main/java/eu/europeana/lod/data/EuropeanaResponse.java
@@ -1,21 +1,31 @@
 package eu.europeana.lod.data;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpServletResponseWrapper;
 
+import com.hp.hpl.jena.query.ResultSet;
+import com.hp.hpl.jena.query.ResultSetFactory;
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.rdf.model.Property;
+import com.hp.hpl.jena.rdf.model.RDFReader;
 import com.hp.hpl.jena.rdf.model.RDFWriter;
 import com.hp.hpl.jena.rdf.model.Resource;
 import com.hp.hpl.jena.rdf.model.Statement;
 import com.hp.hpl.jena.rdf.model.StmtIterator;
+import com.hp.hpl.jena.sparql.engine.http.HttpParams;
 import com.hp.hpl.jena.sparql.engine.http.QueryEngineHTTP;
+import com.hp.hpl.jena.sparql.engine.http.HttpQuery;
+import com.hp.hpl.jena.sparql.util.graph.GraphFactory;
 
 import eu.europeana.lod.util.AcceptHeaderHandler.ContentType;
 import eu.europeana.lod.util.AcceptHeaderHandler.MimeTypePattern;
@@ -84,43 +94,97 @@ public class EuropeanaResponse extends HttpServletResponseWrapper {
 	private Model retrieveModel(String resourceURI, String sparqlEndpoint) {
 
 		// Retrieve the model
+		//String query = "DESCRIBE <" + resourceURI + ">";
+		//&_implicit=false&_equivalent=false&_form=%2Fsparql
+		String tempURI=sparqlEndpoint.substring(0, sparqlEndpoint.lastIndexOf('/')+1)+"resource";
 		String query = "DESCRIBE <" + resourceURI + ">";
-
 		QueryEngineHTTP endpoint = new QueryEngineHTTP(sparqlEndpoint, query);
+		//test cesare
+		HttpQuery hqep=new HttpQuery(tempURI);
+		hqep.addParam("uri", resourceURI);
+		/*hqep.addParam("query", query);
+		hqep.addParam("_implicit", "false");
+		hqep.addParam("_equivalent", "false");*/
+		endpoint.addParam("_implicit", "false");
+		endpoint.addParam("_equivalent", "false");
+		
+		
+		
+		//Model m =GraphFactory.makeJenaDefaultModel();
+		Model m =GraphFactory.makePlainModel();
+		
+		//Model model = endpoint.execDescribe();
+		
+	    //RDFReader arp = m.getReader();
+	  /*
+	    arp.setProperty("error-mode", "lax" );
+	    arp.setProperty("iri-rules", "lax" );
+	    arp.setProperty("WARN_UNQUALIFIED_RDF_ATTRIBUTE","EM_IGNORE");
+	    arp.setProperty("embedding",false);
+	    */
+	    hqep.setAccept(HttpParams.contentTypeRDFXML) ;
+	 // hqep.setAccept(HttpParams.contentTypeResultsXML) ;
+	    InputStream is=hqep.exec();
+	   
+	    /*
+	    InputStreamReader isr = new InputStreamReader(is);
+	    StringBuilder sb=new StringBuilder();
+	    BufferedReader br = new BufferedReader(isr);
+	    String read;
+		try {
+			read = br.readLine();
+			while(read != null) {
+		        System.out.println(read);
+		        sb.append(read);
+		        read =br.readLine();
 
-		Model model = endpoint.execDescribe();
-
+		    }
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+*/
+	    m.read(is, null);
+		//arp.read(m, is, "");
+	   // System.out.println("m "+ m.toString());
+		//end test
+		//System.out.println(endpoint.getContext());
+		//System.out.println(endpoint.toString());
+		//endpoint.addParam("equivalent", "true");
+		
 		// A temporary model for possible BNode descriptions
-		Model bNodeDescriptions = ModelFactory.createDefaultModel();
+	//	Model bNodeDescriptions = ModelFactory.createDefaultModel();
 		
 		// A list of statements having BNodes as values
 		List<Statement> bNodeStatements = new ArrayList<Statement>();
 		
 		// Retrieve the models for all possible blank node values
-		StmtIterator stmtIter = model.listStatements();
+		StmtIterator stmtIter = m.listStatements();
 		while (stmtIter.hasNext()) {
 			Statement stmt = stmtIter.next();
 			if (stmt.getObject().isAnon()) {
-				Resource subject = stmt.getSubject();
+				/*iResource subject = stmt.getSubject();
 				Property property = stmt.getPredicate();
 				Model bNodeDescription = retrieveBNodeDescription(subject,
 						property, sparqlEndpoint);
 				
 				// add BNode description to return model
 				bNodeDescriptions.add(bNodeDescription);
-				
+				*/
 				// store statement for later deletion
 				bNodeStatements.add(stmt);
 
 			}
 		}
-		
-		if (! bNodeDescriptions.isEmpty()) {
-			model.remove(bNodeStatements);
-			model.add(bNodeDescriptions);
-		}
-
-		return model;
+		m.remove(bNodeStatements);
+		//if (! bNodeDescriptions.isEmpty()) {
+		//	model.remove(bNodeStatements);
+		//	model.add(bNodeDescriptions);
+		//}
+		System.out.println("model "+ m.toString());
+		//return model;
+		return m;
 
 	}
 
@@ -133,9 +197,12 @@ public class EuropeanaResponse extends HttpServletResponseWrapper {
 
 		String query = "DESCRIBE ?x WHERE { <" + subject.getURI() + "> <"
 				+ property.getURI() + "> ?x . FILTER (isBlank(?x)) }";
-
+		
+		//&_implicit=false&_equivalent=false&_form=%2Fsparql
+		
 		QueryEngineHTTP endpoint = new QueryEngineHTTP(sparqlEndpoint, query);
-
+		System.out.println("done");
+		
 		Model model = endpoint.execDescribe();
 
 		return model;
